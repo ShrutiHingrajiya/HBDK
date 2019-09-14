683c1f516bb5dd9b55e3af97d0a0f77f762174ae
Shrutih <shrutih.excelsior@gmail.com>
2019-09-14 13:01:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hbd_k/Activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.hbd_k.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hbd_k.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
