8f2deede98e4c0b01b1307721d2103d75fd1476a
Shruti <yogesh.excelsior@gmail.com>
2019-09-16 16:29:30 +0530

    parshva animation

--- app/src/main/java/com/example/hbd_k/Activity/HomeActivity.java
+++ app/src/main/java/com/example/hbd_k/Activity/HomeActivity.java
@@ -31,6 +31,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
         setContentView(R.layout.activity_home);
 
         img = findViewById(R.id.image_view_1);
