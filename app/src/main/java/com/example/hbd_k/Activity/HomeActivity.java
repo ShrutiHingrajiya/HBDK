0e1888049e1fb97863b93ddd35d1a649c86f70c2
ParshvaShah <parshva.excelsior@gmail.com>
2019-09-16 14:06:43 +0530

    parshva

--- app/src/main/java/com/example/hbd_k/Activity/HomeActivity.java
+++ app/src/main/java/com/example/hbd_k/Activity/HomeActivity.java
@@ -1,9 +1,6 @@
 package com.example.hbd_k.Activity;
 
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,13 +16,6 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        img = findViewById(R.id.image_view_glass1);
-     //   ((AnimationDrawable) img.getBackground()).start();
-
-       Animation animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.sequentialanimation);
-
-        img.startAnimation(animFadeIn);
 
     }
 }
