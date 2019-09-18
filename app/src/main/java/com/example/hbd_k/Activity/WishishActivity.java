fa23b1a6536be47ee5835dfeb5f93572f7eb7d34
ParshvaShah <parshva.excelsior@gmail.com>
2019-09-18 16:03:10 +0530

    parshva

---
+++ app/src/main/java/com/example/hbd_k/Activity/WishishActivity.java
@@ -0,0 +1,25 @@
+package com.example.hbd_k.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.etsy.android.grid.StaggeredGridView;
+import com.example.hbd_k.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WishishActivity extends AppCompatActivity {
+
+    @BindView(R.id.grid_view)
+    StaggeredGridView gridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wishish);
+        ButterKnife.bind(this);
+
+    }
+}
