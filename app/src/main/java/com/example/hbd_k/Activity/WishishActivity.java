39a092bf8f5b4083d3bd486bd9270e6bf4ec6694
Yogesh <yogesh.excelsior@gmail.com>
2019-09-19 14:29:32 +0530

    Merge branch 'master' of E:\yogesh\demos\Birthday\HBDK with conflicts.

--- app/src/main/java/com/example/hbd_k/Activity/WishishActivity.java
+++ app/src/main/java/com/example/hbd_k/Activity/WishishActivity.java
@@ -3,14 +3,19 @@ package com.example.hbd_k.Activity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.hbd_k.R;
 
+import butterknife.ButterKnife;
+
 public class WishishActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wishish);
+        ButterKnife.bind(this);
 
     }
 }
